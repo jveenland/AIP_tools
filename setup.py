1da6fa4cf6557c196e4b0b4f6fd7b1a2add576b1
MStarmans91 <m.starmans@erasmusmc.nl>
2022-04-22 11:59:14 +0200

    Added requirements and update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
+# Read requirements file
+with open('requirements.txt', 'r') as fh:
+    _requires = fh.read().splitlines()
+
 setup(
     name="aip",
     version="0.3",
-    description="""TM10005 Week 3 Machine Learning Python Package""",
+    description="""TM11005 Week 3 Machine Learning Python Package""",
     license="Apache 2.0 License",
     author="Martijn Starmans, Jose Castillo Tovar",
     author_email="m.starmans@erasmusmc.nl",
@@ -11,4 +15,5 @@ setup(
     packages=[
         "aip"
     ],
+    install_requires=_requires
 )
