00ca946fa44f614b1f0adf0cba7a0f64853e4767
MStarmans91 <m.starmans@erasmusmc.nl>
2023-04-20 11:52:10 +0200

    Updates in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import find_packages, setup
+import os
 
 # Read requirements file
 with open('requirements.txt', 'r') as fh:
     _requires = fh.read().splitlines()
+
+def scan_dir(path, prefix=None):
+    if prefix is None:
+        prefix = path
+
+    # Scan resources package for files to include
+    file_list = []
+    for root, dirs, files in os.walk(path):
+        # Strip this part as setup wants relative directories
+        root = root.replace(prefix, '')
+        root = root.lstrip('/\\')
+
+        for filename in files:
+            if filename[0:8] == '__init__':
+                continue
+            file_list.append(os.path.join(root, filename))
+
+    return file_list
+
+
+# Determine the extra resources and scripts to pack
+resources_list = scan_dir('./aip/data')
 
 setup(
     name="aip",
@@ -11,11 +35,14 @@ setup(
     license="Apache 2.0 License",
     author="Martijn P. A. Starmans, Jose M. Castillo T.",
     author_email="m.starmans@erasmusmc.nl",
-    package_dir={"": "aip"},
     include_package_data=True,
-    package_data={"aip": ["*.dcm", "*.nii.gz", "*.csv"]},
+    package_data={"aip.data": resources_list},
     packages=[
-        "aip"
+        "aip",
+        "aip.data",
+        "aip.data.registration",
+        "aip.data.registration.Brain",
+        "aip.data.registration.Fundus"
     ],
     install_requires=_requires
 )
