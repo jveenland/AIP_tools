147fe902070b1a3361c9da7f3d6f18512d45714d
jveenland <33731086+jveenland@users.noreply.github.com>
2024-03-27 13:41:26 +0100

    Update __init__.py

--- aip/__init__.py
+++ aip/__init__.py
@@ -1,3 +1,3 @@
 from aip import sitk_helper
-from aip import contour_functions, histogram_features, shape_features, texture_features, other, StatisticalTestThreshold
+from aip import contour_functions, histogram_features, shape_features, texture_features, other, reg_tools, StatisticalTestThreshold
 from aip.data.list_data import list_data, list_data_registration, list_data_reg
